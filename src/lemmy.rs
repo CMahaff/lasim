b7017319bc389088d3a4e0e3d17f63f23fbda575
Connor Mahaffey <connorsmahaffey@gmail.com>
2023-06-17 10:33:35 -0400

    Fix UI bind. Add API calls/libraries to log in.

---
+++ src/lemmy.rs
@@ -0,0 +1,37 @@
+use lemmy_api_common::sensitive::Sensitive;
+use lemmy_api_common::person;
+use lemmy_api_common::site;
+use reqwest::Client;
+use reqwest::Response;
+use reqwest::Error;
+
+
+pub struct API {
+    client: Client,
+}
+
+impl API {
+    pub fn new() -> API {
+        API {
+            client: Client::new(),
+        }
+    }
+
+    pub async fn login(&self, instance: String, username: String, password: String ) -> Result<String, Error> {
+        let params = person::Login {
+            username_or_email: Sensitive::new(username),
+            password: Sensitive::new(password),
+            ..Default::default() // TODO: Add totp_2fa_token for instances with 2-factor
+        };
+    
+        let response: Response = self.client
+            .post(instance + "/api/v3/user/login")
+            .json(&params)
+            .send()
+            .await?;
+    
+        let json = response.json::<person::LoginResponse>().await.unwrap();
+    
+        return Ok(json.jwt.unwrap().to_string());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
+mod lemmy;
+
 use slint::Weak;
+use futures::executor::block_on;
 
 slint::include_modules!();
 
-fn process_download(app_ref: App) {
+#[tokio::main]
+async fn process_download(app_ref: App) {
     let instance = app_ref.get_download_instance_url();
     let username = app_ref.get_download_username_input();
     let password = app_ref.get_download_password_input();
 
-    app_ref.set_download_log_output("Values read!".into());
+    let api = lemmy::API::new();
+
+    let jwt_token_future = api.login(instance.to_string(), username.to_string(), password.to_string());
+    let jwt_token_result = block_on(jwt_token_future);
+
+    if jwt_token_result.is_err() {
+        let error_string = format!("ERROR: Failed Login - {}", jwt_token_result.unwrap_err());
+        app_ref.set_download_log_output(error_string.into());
+        return;
+    }
+
+    let jwt_token = jwt_token_result.unwrap();
+    app_ref.set_download_log_output("Login Success!".into());
 }
 
-fn process_upload(app_ref: App) {
+#[tokio::main]
+async fn process_upload(app_ref: App) {
     let instance = app_ref.get_upload_instance_url();
     let username = app_ref.get_upload_username_input();
     let password = app_ref.get_upload_password_input();
@@ -25,6 +42,7 @@ fn main() {
         let app_weak: Weak<App> = app.as_weak();
 
         move |window_type| {
+            // TODO: We need some kind of threading so this doesn't lock the UI while this happens
             if window_type == "Download" {
                 process_download(app_weak.unwrap());
             } else {
