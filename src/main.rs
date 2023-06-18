424ed42f9a87de1b7fb3ad9e90c4ac4ce0386c29
Connor Mahaffey <connorsmahaffey@gmail.com>
2023-06-18 09:36:12 -0400

    Ignore profile files and pretty-print

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ async fn process_download(processing_instruction: ProcessingInstruction, mut log
         }
     };
 
-    let json_string = serde_json::to_string(&profile_settings);
+    let json_string = serde_json::to_string_pretty(&profile_settings);
     match file.write_all(format!("{}", json_string.unwrap()).as_bytes()) {
         Ok(_) => {
             logger(format!("Wrote Profile to {}", path.to_str().unwrap()))
