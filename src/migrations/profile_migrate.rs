66c2dae8a0d23e7fe82a6fa1ed636febf8d06fac
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-08-01 13:39:24 -0700

    style: apply clippy::unnecessary_unwrap lint

--- src/migrations/profile_migrate.rs
+++ src/migrations/profile_migrate.rs
@@ -10,20 +10,17 @@ pub fn read_latest_profile() -> Result<profile::ProfileConfiguration, String> {
     // Identify latest profile version
     let mut latest_profile_version: u16 = 0;
     let directory_items = std::fs::read_dir("./").unwrap();
-    for item in directory_items {
-        if item.is_ok() {
-            let dir_entry = item.unwrap();
-            let metadata = std::fs::metadata(dir_entry.path());
-            if metadata.is_ok_and(|m| m.is_file()) {
-                let filename = String::from(dir_entry.file_name().to_str().unwrap());
-                if filename.starts_with(PROFILE_FILENAME_START) && filename.ends_with(PROFILE_FILENAME_END) {
-                    let end_index = filename.find(PROFILE_FILENAME_END).unwrap();
-                    let profile_version_string = filename.get(PROFILE_FILENAME_START.char_indices().count()..end_index);
-                    let profile_version = profile_version_string.unwrap().parse::<u16>().unwrap();
+    for dir_entry in directory_items.flatten() {
+        let metadata = std::fs::metadata(dir_entry.path());
+        if metadata.is_ok_and(|m| m.is_file()) {
+            let filename = String::from(dir_entry.file_name().to_str().unwrap());
+            if filename.starts_with(PROFILE_FILENAME_START) && filename.ends_with(PROFILE_FILENAME_END) {
+                let end_index = filename.find(PROFILE_FILENAME_END).unwrap();
+                let profile_version_string = filename.get(PROFILE_FILENAME_START.char_indices().count()..end_index);
+                let profile_version = profile_version_string.unwrap().parse::<u16>().unwrap();
 
-                    if profile_version > latest_profile_version {
-                        latest_profile_version = profile_version;
-                    }
+                if profile_version > latest_profile_version {
+                    latest_profile_version = profile_version;
                 }
             }
         }
@@ -43,8 +40,8 @@ pub fn read_latest_profile() -> Result<profile::ProfileConfiguration, String> {
     }
 
     if latest_profile_version == PROFILE_CURRENT_VERSION {
-        if profile_v1.is_some() {
-            profile_v2 = Some(migrations::migrate_v1_to_v2::convert_profile(profile_v1.unwrap()));
+        if let Some(profile_v1) = profile_v1 {
+            profile_v2 = Some(migrations::migrate_v1_to_v2::convert_profile(profile_v1));
         } else {
             let filename = format!("profile_v{}.json", PROFILE_CURRENT_VERSION);
             let path = Path::new(filename.as_str());
@@ -64,11 +61,7 @@ pub fn read_latest_profile() -> Result<profile::ProfileConfiguration, String> {
         }
     }
 
-    if profile_v2.is_some() {
-        return Ok(profile_v2.unwrap());
-    } else {
-        return Err("ERROR: No saved profiles found. Use download option first!".to_string());
-    }
+    return profile_v2.ok_or_else(|| "ERROR: No saved profiles found. Use download option first!".to_string());
 }
 
 pub fn get_latest_profile_name() -> String {
